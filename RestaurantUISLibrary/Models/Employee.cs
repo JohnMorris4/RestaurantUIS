423e009b490efbb804449d004b4fe55f33c0d70d
JohnMorris4 <johnmorris@morrisje.com>
2018-05-23 16:42:00 -0400

    Setting up initial Models

---
+++ RestaurantUISLibrary/Models/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantUISLibrary.Models
+{
+    class Employee
+    {
+        /// <summary>
+        /// Sets the Id of the employee
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Social Security Number
+        /// </summary>
+        public string SocialSecurityNumber { get; set; }  
+        
+        /// <summary>
+        /// Employee First Name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Employee Last Name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// Employee Address
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// Employee City
+        /// </summary>
+        public string City { get; set; }
+
+        /// <summary>
+        /// Employee State
+        /// </summary>
+        public string State { get; set; }
+
+        /// <summary>
+        /// Employee Zipcode
+        /// </summary>
+        public string ZipCode { get; set; }
+
+        public JobDescriptions JobDescriptions { get; set; }
+
+
+
+    }
+}
---
+++ RestaurantUISLibrary/Models/JobDescriptions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantUISLibrary.Models
+{
+    class JobDescriptions
+    {
+        public string Id { get; set; }
+        public string JobFunction { get; set; }
+       
+    }
+}
